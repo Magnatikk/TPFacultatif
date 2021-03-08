61fc08900b115c35ca3564a6bc744528251508f1
T1masv <timotei.gerante@gmail.com>
2021-03-08 22:13:52 +0100

    tkt woula c recent

--- src/calc/CalculatriceTest.java
+++ src/calc/CalculatriceTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 //import org.junit.Test;
 import org.junit.jupiter.api.Test;
 
-import calc.Calculatrice;
+//import calculatricePile.Calculatrice;
 
 public class CalculatriceTest {
 
