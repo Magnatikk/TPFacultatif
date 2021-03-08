1d79825f2da80dd4ee8a1fb81dfcc27f180f9de8
T1masv <timotei.gerante@gmail.com>
2021-03-08 19:31:42 +0100

    Normalement on ne met pas le ./idea dans le git mais pas grave.

--- src/CalculatriceTest.java
+++ src/CalculatriceTest.java
@@ -1,8 +1,8 @@
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
-//import org.junit.jupiter.api.Test;
+//import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import calculatricePile.Calculatrice;
 
