fc56438e7a46839f332f6321c2955513ba917d5b
Magnatikk <averschuere@iut.univ-paris8.fr>
2021-03-08 19:11:30 +0100

    Test des push

--- src/Calculatrice.java
+++ src/Calculatrice.java
@@ -4,8 +4,8 @@ public class Calculatrice {
     public static void main(String args[]) {
 
         Stack<Double> calculatrice = new Stack<Double>();
-
+        calculatrice.push(3.0);
+        System.out.println(calculatrice.peek());
     }
-
 }
 
