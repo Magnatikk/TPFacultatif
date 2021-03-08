711d274efffd7bd756b65b41176f16dec5aeb827
Magnatikk <averschuere@iut.univ-paris8.fr>
2021-03-08 19:38:39 +0100

    Ajout de chiffres dans la pile

--- src/Calculatrice.java
+++ src/Calculatrice.java
@@ -1,7 +1,24 @@
 import java.util.*;
 
 public class Calculatrice {
-    public
+
+
+
+    public Double entrerValeur() {
+        System.out.println("Rentrez le chiffre que vous voulez rentrer :");
+        Scanner sc = new Scanner(System.in);
+        Double valeur = sc.nextDouble();
+
+        return valeur;
+    }
+
+    public Double sommet() {
+        return calculatrice.peek();
+    }
+
+    public void addition() {
+
+    }
 }
 
 
