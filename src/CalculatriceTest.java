00baaccb4bcd049d8b729b85432d4d96781956b8
Magnatikk <averschuere@iut.univ-paris8.fr>
2021-03-08 19:27:53 +0100

    Test des push

--- src/CalculatriceTest.java
+++ src/CalculatriceTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.Test;
 //import org.junit.jupiter.api.Test;
 
-//import calculatricePile.Calculatrice;
+import calculatricePile.Calculatrice;
 
 public class CalculatriceTest {
 
---
+++ src/main.java
@@ -0,0 +1,12 @@
+import java.util.Stack;
+
+public class main {
+    public static void main(String args[]) {
+
+        Stack<Double> calculatrice = new Stack<Double>();
+        calculatrice.push(3.0);
+        System.out.println(calculatrice.peek());
+
+    }
+}
+
