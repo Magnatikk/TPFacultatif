2e3ab9219bf3df5d5652905243216d29e7bc0f7e
Magnatikk <averschuere@iut.univ-paris8.fr>
2021-03-08 19:07:57 +0100

    Création de la pile

--- src/Calculatrice.java
+++ src/Calculatrice.java
@@ -2,7 +2,9 @@ import java.util.*;
 
 public class Calculatrice {
     public static void main(String args[]) {
-        System.out.println("HAHAHAHAHAHAHA");
+
+        Stack<Double> calculatrice = new Stack<Double>();
+
     }
 
 }
