efcf95d1c8bd93f2be965967cd5435c5de33da79
Magnatikk <averschuere@iut.univ-paris8.fr>
2021-03-08 19:04:59 +0100

    s

--- src/Calculatrice.java
+++ src/Calculatrice.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Calculatrice {
     public static void main(String args[]) {
-        System.out.println();
+        System.out.println("HAHAHAHAHAHAHA");
     }
 
 }
