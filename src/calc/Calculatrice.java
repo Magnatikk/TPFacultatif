c8629179d5023ff853605423c0a1ff7b302db7b6
Magnatikk <averschuere@iut.univ-paris8.fr>
2021-03-09 16:57:18 +0100

    Début du menu de la calculatrice

--- src/calc/Calculatrice.java
+++ src/calc/Calculatrice.java
@@ -1,5 +1,6 @@
 package calc;
 import java.util.*;
+import java.lang.Math;
 
 public class Calculatrice {
     private Stack<Double> calculatrice = new Stack<>();
@@ -9,6 +10,20 @@ public class Calculatrice {
 
     public void entrerValeur(double v) {
         this.calculatrice.push(v);
+    }
+
+    public static double ChiffreOuLettre(String charChoix, double choix) {
+        Boolean flag = Character.isDigit(charChoix.charAt(0));
+        if(flag) {
+            choix = Double.parseDouble(charChoix);
+            return choix;
+        }
+        else {
+            for (int i = 0; i < charChoix.length(); i++) {
+                choix += charChoix.charAt(i);
+            }
+            return choix;
+        }
     }
 
     public int nbValeurs() {
@@ -48,11 +63,19 @@ public class Calculatrice {
         calculatrice.push(resultatAddition);
     }
 
-    public void division() {
+    public int division() {
         double val1 = calculatrice.pop();
         double val2 = calculatrice.pop();
+
+        if (val1 == 0) {
+            calculatrice.push(val2);
+            calculatrice.push(val1);
+            return 1;
+        }
+
         double resultatAddition = val2 / val1;
         calculatrice.push(resultatAddition);
+        return 0;
     }
 
     public void negation() {
@@ -64,6 +87,51 @@ public class Calculatrice {
         while(!calculatrice.empty()) {
             calculatrice.pop();
         }
+    }
+
+    public void sinus() {
+        double val1 = Math.toRadians(calculatrice.pop());
+        calculatrice.push(Math.sin(val1));
+    }
+
+    public void cosinus() {
+        double val1 = Math.toRadians(calculatrice.pop());
+        calculatrice.push(Math.cos(val1));
+    }
+
+    public void tangente() {
+        double val1 = Math.toRadians(calculatrice.pop());
+        calculatrice.push(Math.tan(val1));
+    }
+
+    public void absolue() {
+        double val1 = calculatrice.pop();
+        calculatrice.push(Math.abs(val1));
+    }
+
+    public void logneperien() {
+        double val1 = calculatrice.pop();
+        calculatrice.push(Math.log(val1));
+    }
+
+    public void logBase10() {
+        double val1 = calculatrice.pop();
+        calculatrice.push(Math.log10(val1));
+    }
+
+    public  void logBase2() {
+        double val1 = calculatrice.pop();
+        calculatrice.push((Math.log10(val1) / Math.log(2)));
+    }
+
+    public void  carre() {
+        double val1 = calculatrice.pop();
+        calculatrice.push((val1 * val1));
+    }
+
+    public void racine() {
+        double val1 = calculatrice.pop();
+        calculatrice.push(Math.sqrt(val1));
     }
 }
 
--- src/calc/CalculatriceMenuConsole.java
+++ src/calc/CalculatriceMenuConsole.java
@@ -1,10 +1,107 @@
 package calc;
+import java.util.Scanner;
 
 public class CalculatriceMenuConsole {
 
     public static void menu(Calculatrice c1) {
-        System.out.println("Sommet de la calculatrice : " + c1.sommet());
-        System.out.println("héhé");
+        System.out.println("Sommet de la calculatrice : " + c1.sommet() + "\t\t(" + c1.nbValeurs() + " valeur(s) dans la pile de la calculatrice )");
+        System.out.println("Tapez une valeur pour l'entrer ");
+        System.out.println("a pour addition");
+        System.out.println("s pour soustraction");
+        System.out.println("m pour multiplication");
+        System.out.println("d pour division");
+        System.out.println("n pour négation");
+        System.out.println("sin pour le sinus");
+        System.out.println("cos pour le cosinus");
+        System.out.println("tan pour la tagente");
+        System.out.println("abs pour la valeur absolue");
+        System.out.println("ln pour le logarithme neperien");
+        System.out.println("logDix pour le logarithme en base 10");
+        System.out.println("logDeux pour le logarithme en base 2");
+        System.out.println("carre pour la valeur au carré");
+        System.out.println("racine pour la racine de la valeur");
+        System.out.println("c pour reset la mémoire");
+        System.out.println("q pour quitter");
+        System.out.println("Faites votre choix ;)");
+
+        Scanner sc = new Scanner(System.in);
+        String charChoix = sc.nextLine();
+
+        double choix = 0;
+        choix = Calculatrice.ChiffreOuLettre(charChoix, choix);
+
+
+
+        while ((int)choix != 113) {
+            if ((int)choix == 97) {
+                c1.addition();
+                menu(c1);
+            }
+            else if ((int)choix == 115) {
+                c1.soustraction();
+                menu(c1);
+            }
+            else if ((int)choix == 109) {
+                c1.multiplication();
+                menu(c1);
+            }
+            else if ((int)choix == 100) {
+                c1.division();
+                menu(c1);
+            }
+            else if ((int)choix == 110) {
+                c1.negation();
+                menu(c1);
+            }
+            else if ((int)choix == 99) {
+                c1.effacer();
+                menu(c1);
+            }
+            else if ((int)choix == 113) {
+                System.exit(0);
+            }
+            else if ((int)choix == 330) {
+                c1.sinus();
+                menu(c1);
+            }
+            else if ((int)choix == 325) {
+                c1.cosinus();
+                menu(c1);
+            }
+            else if ((int)choix == 323) {
+                c1.tangente();
+                menu(c1);
+            }
+            else if ((int)choix == 310) {
+                c1.absolue();
+                menu(c1);
+            }
+            else if ((int)choix == 218) {
+                c1.logneperien();
+                menu(c1);
+            }
+            else if ((int)choix == 615) {
+                c1.logBase10();
+                menu(c1);
+            }
+            else if ((int)choix == 728) {
+                c1.logBase2();
+                menu(c1);
+            }
+            else if ((int)choix == 525) {
+                c1.carre();
+                menu(c1);
+            }
+            else if ((int)choix == 626) {
+                c1.racine();
+                menu(c1);
+            }
+            else {
+                c1.entrerValeur(choix);
+                menu(c1);
+            }
+        }
 
     }
+
 }
